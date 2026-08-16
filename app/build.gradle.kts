fc9996ab1a3fa56c7cb828f525bc2c98776e5c67
jangalacharansaisri963-eng <jangalacharansaisri963@gmail.com>
2026-08-16 15:59:16 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,16 +70,6 @@ android {
     }
 }
 
-chaquopy {
-    productFlavors {
-        create("main") {
-            sourceSets.getByName("main") {
-                srcDir("src/main/python")
-            }
-        }
-    }
-}
-
 // Configure the Secrets Gradle Plugin to use .env and .env.example files
 // to match the convention used in Web projects.
 secrets {
